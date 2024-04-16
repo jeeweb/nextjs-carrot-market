331f2a1d67a1848129b3fa416b39f29b68cc5c96
jeeweb <52396673+jeeweb@users.noreply.github.com>
2024-04-16 19:55:58 +0900

    feat: login form submit 구현

---
+++ pages/api/users/enter.tsx
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "../../../libs/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    res.status(401).end(); // bad request
+  }
+  console.log(req.body.email); // 로그인 시도한 이메일 가져오기
+  res.status(200).end();
+}
