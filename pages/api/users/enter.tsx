c5d9bb885b631546c56a1ea6bbd0941aad94b8a5
jeeweb <52396673+jeeweb@users.noreply.github.com>
2024-04-17 13:43:28 +0900

    refactor: useMutation으로 변경

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import client from "../../../libs/client";
+import client from "../../../libs/server/client";
 
 export default async function handler(
   req: NextApiRequest,
@@ -8,6 +8,6 @@ export default async function handler(
   if (req.method !== "POST") {
     res.status(401).end(); // bad request
   }
-  console.log(req.body.email); // 로그인 시도한 이메일 가져오기
+  console.log(req.body); // 로그인 시도한 이메일 가져오기
   res.status(200).end();
 }
