ff7a9396dc1522dd8b650e867362eb766317788d
jeeweb <52396673+jeeweb@users.noreply.github.com>
2024-04-17 23:41:00 +0900

    refactor: prisma upsert

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -4,48 +4,27 @@ import client from "@libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body; // phone 혹은 email 둘 중 하나의 데이터를 가짐
-  let user;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+      /*
+        ...(phone && { phone: +phone }),  // ...(phone ? { phone: +phone }: {}),
+        ...(email && { email })           // ...(email ? { email }: {}),
+      */
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+      /*
+        ...(phone && { phone: +phone }),  // ...(phone ? { phone: +phone }: {}),
+        ...(email && { email })           // ...(email ? { email }: {}),
+      */
+    },
+    update: {},
+  });
+  console.log(user);
 
-  // Email로 로그인할 때
-  if (email) {
-    user = await client.user.findUnique({
-      where: {
-        email,
-      },
-    });
-    if (user) console.log("found it.");
-    if (!user) {
-      console.log("Did not find. Will create.");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          email,
-        },
-      });
-    }
-    console.log(user);
-  }
-
-  // phone번호로 로그인할 때
-  if (phone) {
-    user = await client.user.findUnique({
-      where: {
-        // phone,   // 입력된 phone 데이터를 그냥 확인하면 string으로 들어오기 때문에 타입에러 발생
-        phone: +phone, // string을 간단하게 number로 바꿔줌
-      },
-    });
-    if (user) console.log("found it.");
-    if (!user) {
-      console.log("Did not find. Will create.");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          phone: +phone, // string을 간단하게 number로 바꿔줌
-        },
-      });
-    }
-    console.log(user);
-  }
   return res.status(200).end();
 }
 
